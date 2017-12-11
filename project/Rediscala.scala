35d27e2e5c5a22f352f0667787035749c27daf70
Juan Miguel Cejuela <i@juanmi.rocks>
2017-12-11 13:31:47 +0100

    update scala 2.11.x and 2.12.y

--- project/Rediscala.scala
+++ project/Rediscala.scala
@@ -51,8 +51,8 @@ object RediscalaBuild extends Build {
     Seq(
       name := "rediscala",
       organization := "com.github.etaty",
-      scalaVersion := "2.11.8",
-      crossScalaVersions := Seq(scalaVersion.value, "2.12.0"),
+      scalaVersion := "2.11.11",
+      crossScalaVersions := Seq(scalaVersion.value, "2.12.4"),
       licenses += ("Apache-2.0", url("http://www.apache.org/licenses/LICENSE-2.0.html")),
       homepage := Some(url("https://github.com/etaty/rediscala")),
       scmInfo := Some(ScmInfo(url("https://github.com/etaty/rediscala"), "scm:git:git@github.com:etaty/rediscala.git")),
